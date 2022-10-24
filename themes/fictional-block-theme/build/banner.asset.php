7e7745a2c87cbd3a390b804bccd917c238656e76
Jake Bogan <jakebogan01@gmail.com>
2022-10-24 18:52:53 -0400

    adds banner block

---
+++ themes/fictional-block-theme/build/banner.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-element'), 'version' => '08bbef829e7123cfc4a1');
---
+++ themes/fictional-block-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '77a3eb593cfdfdb14f5e');
